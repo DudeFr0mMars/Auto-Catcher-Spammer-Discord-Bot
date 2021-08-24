11ac3730184aad61f254b5a7a37a5db31a4d41dd
DudeFr0mMars <84705014+DudeFr0mMars@users.noreply.github.com>
2021-08-24 23:05:15 +0530

    Update spammer.py

--- spammer.py
+++ spammer.py
@@ -14,6 +14,17 @@ TOKEN = "Enter TOKEN Here"
 cid = "Enter channel id without quotes"
 #example: cid = 8778172817281
 
+f = open('pokes.txt', 'r')
+file = f.read()
+file = file.split('\n')
+for i in file:
+    if len(i) < 2:
+        del file[file.index(i)]
+pokemons = []
+for i in file:
+    if len(i) >= 3:
+        pokemons.append(i)
+
 req = requests.get("https://discord.com/api/path/to/the/endpoint")
 
 print(req)
@@ -46,6 +57,63 @@ async def on_message(message):
           response = await bot.wait_for('message', check = check, timeout=300) 
           if "The pokémon is" in response.content:
             break
+
+hint = ""
+@bot.event
+async def on_message(message):
+        
+      def check(m):
+          return m.channel == message.channel and m.author != bot.user and "The pokémon is" in m.content
+      global hint
+      embeds = message.embeds
+
+      if not message.embeds:
+        await bot.process_commands(message)
+        return
+      
+      title = (embeds[0].to_dict()['title'])   
+      if "pokémon has appeared" in title:
+        hint = ""
+        m = await message.channel.send(".hint")
+        
+        while True:
+          response = await bot.wait_for('message', check = check, timeout=300) 
+          if "The pokémon is" in response.content:
+            break
+        
+      s = response.content.split(" is ")[1].replace(".","")
+      print(s)
+      x = get_mon(s)
+      print(x)
+      first_options = x
+      for i in first_options:
+        await message.channel.send(".catch "+i)
+ 
+def get_mon(val):
+ val = val.lower()
+ while "\_" in val:
+     val = val.replace("\_", "-")
+ length = len(val)
+ l = list(val)
+ new_chars = []
+ with open('pokemon.txt') as f:
+     lines = [line.rstrip() for line in f]
+ new_names = []
+ for i in lines:
+   if len(i) == length:
+     new_names.append(i)
+ final_list = []
+ for i in new_names:
+   name_list = list(i)
+   index = 0
+   flag = False
+   for k in l:
+     if name_list[index] != k and k != "-":
+       flag = True
+     index = index+1
+   if not flag:
+     final_list.append(i)
+ return final_list
 
 @bot.command()
 async def say(ctx, *, args):
